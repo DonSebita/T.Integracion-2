123cf7621f676187d34a7bacf442fe855c9741f1
Christian Verdugo <crischandel@gmail.com>
2023-08-27 16:07:28 -0400

    Pagina home, sucursales

---
+++ pages/components/footbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footbar = (props) => {
+    return (
+        <div className={props.className + "h-min bg-slate-700"}>
+            <h1>FOOTBAR</h1>
+        </div>
+    );
+}
+
+export default Footbar;
+
---
+++ pages/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <div className={props.className + " h-min bg-slate-100"}>
+            <h1 className="text-black">NAVBAR</h1>
+        </div>
+    );
+}
+
+export default Navbar;
