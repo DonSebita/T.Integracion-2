823f9215d22fe93d51eda1a5f0e463bda47453a8
sebastian lucas lopez <luxioxbox49@gmail.com>
2023-10-11 21:36:06 -0300

    comienzo del apartado de abogados
    el login queda aparte para hacerlo y despues integrarlo

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
-    experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] },
-    webpack(config) {
-        config.experiments = { ...config.experiments, topLevelAwait: true };
-        return config;
-    }
+
+const nextConfig = {
+
 }
+ 
+module.exports = nextConfig
