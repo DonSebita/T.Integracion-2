e560c6f3ef2c182e9b5144b64e58445fe231c3cb
Christian Verdugo <crischandel@gmail.com>
2023-11-20 22:00:13 -0300

    Prueba servidor

--- components/login-btn.jsx
+++
@@ -1,9 +0,0 @@
-"use server"
-import { useSession, signIn, signOut } from "next-auth/react"
-
-export default async function Component() {
-  const { data } = useSession()
-  const { accessToken } = data
-
-  return <div>Access Token: {accessToken}</div>
-}
